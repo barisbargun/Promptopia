eb0e959bff5406887e9e57fadb2933b0ec4fe2cb
barış olgun <ben_8_10@hotmail.com>
2024-01-27 20:16:27 +0300

    eighth

--- app/api/user/[id]/post/route.js
+++ app/api/user/[id]/post/route.js
@@ -7,7 +7,6 @@ export const GET = async (req, { params }) => {
   try {
     await connectToDB();
     const res = await Post.find({ creator: id }).populate('creator');
-    if (!res) throw Error;
     return new Response(JSON.stringify(res), { status: 200 });
   } catch (error) {
     return responseMessages("ServerError");
